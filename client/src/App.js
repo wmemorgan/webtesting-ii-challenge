ed970161b70f40e563c0c56abef84c2b326e7aeb
Wilfred Morgan <wil.me.morgan@gmail.com>
2019-06-24 19:42:37 +0800

    Import Dashboard component to App

--- client/src/App.js
+++ client/src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 
 import AppContainer from './components/DesignComponents/AppStyles'
 import Display from './components/ScoreBoardComponents/Display'
+import Dashboard from './components/ScoreBoardComponents/Dashboard'
 
 function App() {
   return (
     <AppContainer>
       <Display />
+      <Dashboard />
     </AppContainer>
   );
 }
