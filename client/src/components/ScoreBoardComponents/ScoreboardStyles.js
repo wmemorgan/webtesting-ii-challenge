c62a306fd7c3634f1c7fa1318b55989fecf8173d
Wilfred Morgan <wil.me.morgan@gmail.com>
2019-06-24 18:12:15 +0800

    Add format and styling for Scoreboard components

---
+++ client/src/components/ScoreBoardComponents/ScoreboardStyles.js
@@ -0,0 +1,42 @@
+import styled, { css } from 'styled-components'
+import { color, colorScheme, fontSizing, flex } from '../DesignComponents/theme'
+
+export const ScoreboardContainer = styled.div`
+  width: 100%;
+  max-width: 600px;
+  ${flex('column', 'center', 'center')};
+  margin: 10px auto;
+  border: 1px solid ${colorScheme.defaultBorderColor};
+  border-radius: 5px;
+`
+
+export const StatContainer = styled.div`
+  width: 90%;
+  ${flex('row','center','space-between')}
+
+`
+
+export const StatWrapper = styled.div`
+  width: 100%;
+  text-align: center;
+`
+
+export const StatCategory = styled.div`
+  margin: 5px 0;
+  font-size: ${fontSizing.s}
+  text-transform: uppercase;
+`
+
+export const StatDisplay = styled.div`
+  margin: 5px 0;
+  font-size: ${fontSizing.m}
+  ${props => props.strike &&
+    css`
+      color: ${color.danger};
+  `}
+
+  ${props => props.ball &&
+    css`
+      color: ${color.accent};
+  `}
+`
