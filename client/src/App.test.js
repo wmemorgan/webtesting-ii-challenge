587b026de5e82471b005075b3c89c6fbe7d33c87
Wilfred Morgan <wil.me.morgan@gmail.com>
2019-06-24 18:09:38 +0800

    Incorporate Display component into main App component

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,9 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import { render } from '@testing-library/react'
+import '@testing-library/react/cleanup-after-each'
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+// Components
+import App from './App';
+import Display from './components/ScoreBoardComponents/Display'
+//import Dashboard from './components/Dashboard'
+
+describe('Test <App /> component', () => {
+  it('<App /> renders without crashing', () => {
+    render(<App />)
+  })
+
+  it('<Display /> renders without crashing', () => {
+    render(<Display />)
+  })
+
+  // xit('Dashboard renders without crashing', () => {
+  //   render(<Dashboard />)
+  // })
+})
+
+
