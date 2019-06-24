668bff7dc57101daff7473bb3329151ed57a348e
Wilfred Morgan <wil.me.morgan@gmail.com>
2019-06-24 19:41:28 +0800

    Add buttons and relevant unit tests to Dashboard

--- client/src/components/ScoreBoardComponents/Dashboard.js
+++ client/src/components/ScoreBoardComponents/Dashboard.js
@@ -1,11 +1,22 @@
-import React, { Component } from 'react'
+import React from 'react'
+import { EventEmitter } from '../../utils/events'
 
-class Dashboard extends Component {
-  render() {
-    return (
-      <h2>Dashboard</h2>
-    )
-  }
+const addBall = () => {
+  EventEmitter.dispatch('addBall')
+}
+
+const addStrike = () => {
+  EventEmitter.dispatch('addStrike')
+}
+
+const Dashboard = () => {
+  return (
+    <div>
+      <button onClick={addBall}>Ball</button>
+      <button onClick={addStrike}>Strike</button>
+      <button>Foul</button>
+    </div>
+  )
 }
 
 export default Dashboard
--- client/src/components/ScoreBoardComponents/Dashboard.test.js
+++ client/src/components/ScoreBoardComponents/Dashboard.test.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { render } from '@testing-library/react'
+import { render, fireEvent } from '@testing-library/react'
 import '@testing-library/react/cleanup-after-each'
 
 import Dashboard from './Dashboard'
@@ -7,5 +7,32 @@ import Dashboard from './Dashboard'
 describe('Test <Dashboard /> component', () => {
   it('renders without errors', () => {
     render(<Dashboard />)
+    // console.log(`\n***Rendered Dashboard***`, render(<Dashboard />).debug())
+  })
+})
+
+describe('Test buttons', () => {
+  it('Ball button', () => {
+    const { getByText } = render(<Dashboard />)
+    const ballButton = getByText(/ball/i)
+
+    fireEvent.click(ballButton, 'click')
+
+  })
+
+  it('Strike button', () => {
+    const { getByText } = render(<Dashboard />)
+    const strikeButton = getByText(/strike/i)
+
+    fireEvent.click(strikeButton, 'click')
+
+  })
+
+  it('Foul button', () => {
+    const { getByText } = render(<Dashboard />)
+    const foulButton = getByText(/foul/i)
+
+    fireEvent.click(foulButton, 'click')
+
   })
 })
