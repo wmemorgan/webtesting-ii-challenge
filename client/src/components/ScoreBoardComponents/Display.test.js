c36dc4b1cf4034db98f74fdbbd1ea1f54ddb8831
Wilfred Morgan <wil.me.morgan@gmail.com>
2019-06-26 19:48:31 +0800

    Add test suite to validate props rendering

--- client/src/components/ScoreBoardComponents/Display.test.js
+++ client/src/components/ScoreBoardComponents/Display.test.js
@@ -4,28 +4,72 @@ import '@testing-library/react/cleanup-after-each'
 
 import Display from './Display'
 
-describe('Test <Display /> component', () => {
-  it('should render without errors', () => {
-    render(<Display />)
+describe('renders <Display /> component', () => {
+  describe('display stat categories', () => {
+    it('should render without errors', () => {
+      render(<Display />)
+    })
+
+    it('should display "Scoreboard"', () => {
+      const { getByText } = render(<Display />)
+      getByText(/scoreboard/i)
+    })
+
+    it('should display "Balls"', () => {
+      const { getByText } = render(<Display />)
+      getByText(/balls/i)
+    })
+
+    it('should display "Strikes"', () => {
+      const { getByText } = render(<Display />)
+      getByText(/strikes/i)
+    })
+
+    it('should display "Outs"', () => {
+      const { getByText } = render(<Display />)
+      getByText(/outs/i)
+    })
   })
 
-  it('should display "Scoreboard"', () => {
-    const { getByText } = render(<Display />)
-    getByText(/scoreboard/i)
-  })
+  describe('display game data', () => {
+    it('display number of balls', () => {
+      const mockState = { balls: 0 }
+      const component = render(<Display {...mockState} />)
+      const numberOfBalls = component.getByTestId(/ball-count/i).textContent
 
-  it('should display "Balls"', () => {
-    const { getByText } = render(<Display />)
-    getByText(/balls/i)
-  })
+      expect(Number(numberOfBalls)).toBe(mockState.balls)
+    })
 
-  it('should display "Strikes"', () => {
-    const { getByText } = render(<Display />)
-    getByText(/strikes/i)
-  })
+    it('display number of strikes', () => {
+      const mockState = { strikes: 2 }
+      const component = render(<Display {...mockState} />)
+      const numberOfStrikes = component.getByTestId(/strike-count/i).textContent
 
-  it('should display "Outs"', () => {
-    const { getByText } = render(<Display />)
-    getByText(/outs/i)
+      expect(Number(numberOfStrikes)).toBe(mockState.strikes)
+    })
+
+    it('display number of outs', () => {
+      const mockState = {
+        balls: 0,
+        strikes: 0,
+        outs: 2
+      }
+      const component = render(<Display {...mockState} />)
+      const numberOfOuts = component.getByTestId(/out-count/i).textContent
+
+      expect(Number(numberOfOuts)).toBe(mockState.outs)
+    })
+
+    it('display number of hits', () => {
+      const mockState = {
+        balls: 0,
+        strikes: 0,
+        hits: 1
+      }
+      const component = render(<Display {...mockState} />)
+      const numberOfHits = component.getByTestId(/hit-count/i).textContent
+
+      expect(Number(numberOfHits)).toBe(mockState.hits)
+    })
   })
 })
