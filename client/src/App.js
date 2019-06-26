22f06d1308687e261792e9d77972603d1a3eabce
Wilfred Morgan <wil.me.morgan@gmail.com>
2019-06-26 19:47:39 +0800

    Refactor App:
    -Move app state and methods to <App/> component
    
    -Pass state as props to <Display/> component
    
    -Add test ids to stat elements

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,103 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { EventEmitter } from './utils/events'
 
 import AppContainer from './components/DesignComponents/AppStyles'
 import Display from './components/ScoreBoardComponents/Display'
 import Dashboard from './components/ScoreBoardComponents/Dashboard'
 
-function App() {
-  return (
-    <AppContainer>
-      <Display />
-      <Dashboard />
-    </AppContainer>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      balls: 0,
+      strikes: 0,
+      fouls: 0,
+      outs: 0,
+      hits: 0,
+      walks: 0
+    }
+
+    EventEmitter.subscribe('addBall', event => this.addBall(event))
+    EventEmitter.subscribe('addStrike', event => this.addStrike(event))
+    EventEmitter.subscribe('addFoul', event => this.addFoul(event))
+    EventEmitter.subscribe('addHit', event => this.addHit(event))
+  }
+
+  addBall = () => {
+    if (this.state.balls <= 3) {
+      this.setState(prevState => (
+        { balls: prevState.balls + 1 }
+      ), () => console.log(this.state))
+    } else {
+      this.setState({
+        balls: 0,
+        strikes: 0,
+        fouls: 0
+      })
+    }
+  }
+
+  addStrike = () => {
+    if (this.state.strikes <= 2) {
+      this.setState(prevState => (
+        { strikes: prevState.strikes + 1 }
+      ))
+    } else {
+      this.setState(prevState => (
+        {
+          balls: 0,
+          strikes: 0,
+          fouls: 0,
+          outs: prevState.outs + 1
+        }
+      ))
+    }
+  }
+
+  addFoul = () => {
+    if (this.state.strikes < 2) {
+      this.setState(prevState => (
+        {
+          strikes: prevState.strikes + 1,
+          fouls: prevState.fouls + 1
+        }
+      ), () => console.log(this.state))
+    } else {
+      this.setState(prevState => (
+        { fouls: prevState.fouls + 1 }
+      ), () => console.log(this.state))
+    }
+  }
+
+  addHit = () => {
+    this.setState(prevState => (
+      {
+        balls: 0,
+        strikes: 0,
+        fouls: 0,
+        hits: prevState.hits + 1
+      }
+    ))
+  }
+
+  componentDidUpdate() {
+    if (this.state.balls === 4) {
+      this.addBall()
+    }
+    if (this.state.strikes === 3) {
+      this.addStrike()
+    }
+
+  }
+
+  render() {
+    return (
+      <AppContainer>
+        <Display {...this.state}/>
+        <Dashboard />
+      </AppContainer>
+    )
+  }
 }
 
 export default App;
--- client/src/components/ScoreBoardComponents/Display.js
+++ client/src/components/ScoreBoardComponents/Display.js
@@ -1,133 +1,48 @@
-import React, { Component } from 'react'
-import { EventEmitter } from '../../utils/events'
+import React from 'react'
+
 import * as S from './ScoreboardStyles'
 
-class Display extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      balls: 0,
-      strikes: 0,
-      fouls: 0,
-      outs: 0,
-      hits: 0,
-      walks: 0
-    }
-
-    EventEmitter.subscribe('addBall', event => this.addBall(event))
-    EventEmitter.subscribe('addStrike', event => this.addStrike(event))
-    EventEmitter.subscribe('addFoul', event => this.addFoul(event))
-    EventEmitter.subscribe('addHit', event => this.addHit(event))
-
-  }
-
-  addBall = () => {
-    if (this.state.balls <= 3) {
-      this.setState(prevState => (
-        { balls: prevState.balls + 1 }
-      ), () => console.log(this.state))
-    } else {
-      this.setState({
-        balls: 0,
-        strikes: 0,
-        fouls: 0
-      })
-    }
-  }
-
-  addStrike = () => {
-    if (this.state.strikes <= 2) {
-      this.setState(prevState => (
-        { strikes: prevState.strikes + 1 }
-      ))
-    } else {
-      this.setState(prevState => (
-        {
-          balls: 0,
-          strikes: 0,
-          fouls: 0,
-          outs: prevState.outs + 1
-        }
-      ))
-    }
-  }
-
-  addFoul = () => {
-    if (this.state.strikes < 2) {
-      this.setState(prevState => (
-        { strikes: prevState.strikes + 1, 
-          fouls: prevState.fouls + 1
-        }
-      ), () => console.log(this.state))
-    } else {
-      this.setState(prevState => (
-        { fouls: prevState.fouls + 1 }
-      ), () => console.log(this.state))
-    }
-  }
-
-  addHit = () => {
-    this.setState(prevState => (
-      { 
-        balls: 0,
-        strikes: 0,
-        fouls: 0,
-        hits: prevState.hits + 1 
-      }
-    ))
-  }
-
-  componentDidUpdate() {
-    if (this.state.balls === 4) {
-      this.addBall()
-    }
-    if (this.state.strikes === 3) {
-      this.addStrike()
-    }
-
-  }
-
-  render() {
-    return (
-      <S.ScoreboardContainer>
-        <h2>Scoreboard</h2>
-        <S.StatContainer>
-          <S.StatWrapper>
-            <S.StatCategory>
-              Balls
+const Display = props => {
+  const { balls, strikes, outs, hits } = props
+  return (
+    <S.ScoreboardContainer>
+      <h2>Scoreboard</h2>
+      <S.StatContainer>
+        <S.StatWrapper>
+          <S.StatCategory>
+            Balls
             </S.StatCategory>
-            <S.StatDisplay>
-              {this.state.balls}
-            </S.StatDisplay>
-          </S.StatWrapper>
-          <S.StatWrapper>
-            <S.StatCategory>
-              Strikes
+          <S.StatDisplay data-testid='ball-count'>
+            {balls}
+          </S.StatDisplay>
+        </S.StatWrapper>
+        <S.StatWrapper>
+          <S.StatCategory>
+            Strikes
             </S.StatCategory>
-            <S.StatDisplay>
-              {this.state.strikes}
-            </S.StatDisplay>
-          </S.StatWrapper>
-          <S.StatWrapper>
-            <S.StatCategory>
-              Outs
+          <S.StatDisplay data-testid='strike-count'>
+            {strikes}
+          </S.StatDisplay>
+        </S.StatWrapper>
+        <S.StatWrapper>
+          <S.StatCategory>
+            Outs
             </S.StatCategory>
-            <S.StatDisplay>
-              {this.state.outs}
-            </S.StatDisplay>
-          </S.StatWrapper>
-          <S.StatWrapper>
-            <S.StatCategory>
-              Hits
+          <S.StatDisplay data-testid='out-count'>
+            {outs}
+          </S.StatDisplay>
+        </S.StatWrapper>
+        <S.StatWrapper>
+          <S.StatCategory>
+            Hits
             </S.StatCategory>
-            <S.StatDisplay>
-              {this.state.hits}
-            </S.StatDisplay>
-          </S.StatWrapper>
-        </S.StatContainer>
-      </S.ScoreboardContainer>
-    )
-  }
+          <S.StatDisplay data-testid='hit-count'>
+            {hits}
+          </S.StatDisplay>
+        </S.StatWrapper>
+      </S.StatContainer>
+    </S.ScoreboardContainer>
+  )
 }
 
 export default Display
